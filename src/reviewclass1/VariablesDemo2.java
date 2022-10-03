bdf622718fbbd10a2d19fddb406e63994be89401
erikmartinez93 <erkmartinez93@gmail.com>
2022-10-02 22:47:57 -0400

    first commit

---
+++ src/reviewclass1/VariablesDemo2.java
@@ -0,0 +1,22 @@
+package reviewclass1;
+
+public class VariablesDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * float
+		 * double //most widely used 
+		 */
+		float box1=12.5f; // create a box type float and store 12.5 in it the f at the end is part
+		double box2=15.6;
+		
+		box1=10; // reassigning 10 to box1
+		box1=box1+5; // i am again changing the value of box1 
+		System.out.println(box1);
+		System.out.println(box2);
+
+	}
+
+}
